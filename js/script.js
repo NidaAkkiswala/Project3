bef89cc858d3bb5c3c4f03abd3a933e4da66e3d6
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2021-04-27 19:15:34 -0500

    added map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
       $('.slider').bxSlider();
     });
+	
+function initMap(): {
+	const myLatLng = {lat: 42.052648121921564, lng: -87.75195584135642}
+	
+	const map = new google.maps.Map{
+		document.getElementById("map") as HTMLElement,
+		{
+			zoom: 4,
+			center: myLatLng,
+		}
+	};
+	
+	new google.maps.Marker({
+		postion: myLatLng,
+		map,
+		title: "home",
+	});
+}
+		
+	
+
