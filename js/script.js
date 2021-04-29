58ecabc70cfb0831f433ec60071313758d5c1ffb
Nida Akkiswala <nakkiswala@hawk.iit.edu>
2021-04-28 21:30:10 -0500

    fixed map

--- js/script.js
+++ js/script.js
@@ -2,23 +2,35 @@ $(document).ready(function(){
       $('.slider').bxSlider();
     });
 	
-function initMap(): {
-	const myLatLng = {lat: 42.052648121921564, lng: -87.75195584135642}
-	
-	const map = new google.maps.Map{
-		document.getElementById("map") as HTMLElement,
-		{
-			zoom: 4,
-			center: myLatLng,
-		}
+function initMap() {
+	var el = document.getElementById('canvas');
+	var myLatLng = new google.maps.LatLng(42.052648121921564, -87.75195584135642);
+	var MapOptions = {
+		center: myLatLng,
+		zoom: 10
 	};
 	
-	new google.maps.Marker({
+	var myMap = new google.maps.Map(el, MapOptions);
+	
+	
+	var marker = new google.maps.Marker({
 		postion: myLatLng,
-		map,
-		title: "home",
+		map: myMap,
+		icon: 'images/house.png'
+	});
+	
+	var contentString = '<h1>My Home</h1><p>No place better than a good home</p>';
+	
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	});
+	
+	google.maps.event.addListener(marker, 'mouseover', function() {
+		infowindow.open(myMap, marker);
 	});
 }
+
+google.maps.event.addDomListener(window, 'load', initMap);
 		
 	
 
